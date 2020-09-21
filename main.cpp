46b655f5d74ff6a678eff9c07e387c31c8a19b73
luciana1faur <luciana_faur@yahoo.com>
2020-09-21 05:51:52 +0000

    i = 0 dupa int i;

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main() {
   char s[200];
   int i;
+  i = 0;
+  
   cout << "Introduceti un sir de caractere de la tastatura, fara spatii: ";
   cin >> s;
-  i = 0;
+ 
   while (s[i] != 0)
-    i++;
+   i++;
   cout << "Sirul are " << i << " caractere." << endl;
   return 0;
 }
